99220745313c4b088f713f94d3515f6e86463a42
Pencroff <pencroff@gmail.com>
2012-10-08 14:48:55 +0300

    Correct todos collections

--- backbone-require/js/collections/todos.js
+++ backbone-require/js/collections/todos.js
@@ -11,7 +11,7 @@ define([
 		model: Todo,
 
 		// Save all of the todo items under the `"todos"` namespace.
-		localStorage: new Store('todos-backbone'),
+		localStorage: new Backbone.LocalStorage('todos-backbone'),
 
 		// Filter down the list of all todo items that are finished.
 		completed: function () {
